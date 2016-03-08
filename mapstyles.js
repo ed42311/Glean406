cdf99bec16837c104ad9c833fae8a519e3a8561c
Lars Finley Martinson <larsm_eyes@hotmail.com>
2016-03-08 12:20:39 -0700

    map integrated

---
+++ mapstyles.js
@@ -0,0 +1,34 @@
+var mapStyles = [{
+    featureType: 'poi',
+    stylers: [{ visibility: 'off' }]  
+   },{
+    featureType: 'transit.station',
+    stylers: [{ visibility: 'off' }]  
+   },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [{ visibility: "on" }]
+   },{
+    "featureType": "poi.park",
+    "stylers": [{ "visibility": "on" }]
+   },{
+    "featureType": "poi.attraction",
+    "stylers": [{ "visibility": "off" }]
+  },{
+    "featureType": "poi.business",
+    "stylers": [{ "visibility": "off" }]
+  },{
+    "featureType": "poi.government",
+    "stylers": [{ "visibility": "off" }]
+  },{
+    "featureType": "poi.medical",
+    "stylers": [{ "visibility": "off" }]
+  },{
+    "featureType": "poi.place_of_worship",
+    "stylers": [{ "visibility": "off" }]
+  },{
+  	    disableDoubleClickZoom: false;
+  }
+];
+
+module.exports = mapStyles;
