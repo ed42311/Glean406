0b342c72b7060e0e01117ecf02342bdcb4a0e031
jerecockrell <jere.cockrell@gmail.com>
2016-03-10 08:53:24 -0700

    add routes, update server.js, navbar positioning and views pages

---
+++ routes/user.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
+// app/routes.js
+module.exports = function(app, passport) {
+
+   app.post('/signup', passport.authenticate('local-signup', {
+       successRedirect: '/', // redirect to the secure profile section
+       failureRedirect: '/signup', // redirect back to the signup page if there is an error
+       failureFlash: true // allow flash messages
+   }));
+
+   app.get('/signup', function(req, res){
+       res.render('signup')
+   })
+
+   app.post('/login', passport.authenticate('local-login', {
+       successRedirect: '/', // redirect to the secure profile section
+       failureRedirect: '/login', // redirect back to the signup page if there is an error
+       failureFlash: true // allow flash messages
+   }));
+
+   app.get('/login', function(req, res) {
+       // render the page and pass in any flash data if it exists
+       res.render('login.ejs', {
+           message: req.flash('loginMessage')
+       });
+   });
+
+
+   app.get('/signup', function(req, res) {
+       // render the page and pass in any flash data if it exists
+       res.render('signup.ejs', {
+           message: req.flash('signupMessage')
+       });
+   });
+
+   app.get('/logout', function(req, res) {
+       req.logout();
+       res.redirect('/');
+   });
+
+
+   // app.get('/admin', isAdmin, function(req, res) {
+   //     mongoose.model('User').find({}, function(err, users) {
+   //         if (err) {
+   //             return console.log(err);
+   //         } else {
+   //             res.render('adminProfile.ejs', {
+   //                 users: users,
+   //                 user: req.user
+   //             });
+   //         }
+   //     });
+
+
+   // });
+};
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ mongoose.connect('mongodb://localhost/forageposts');
 var router = express.Router();
 var port = process.env.PORT || 8080;
 var floraRoutes = require('./routes/flora');
+var userRoutes = require('./routes/user');
+var session = require('express-session');
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
@@ -14,16 +16,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// required for passport
-// app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
 
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(session({
+cookie: {
+  maxAge: 60000
+}
+}));
 
+require('./config/passport')(passport);
+require('./routes/user.js')(app, passport);
 
+app.use(function(req, res, next) {
+    var user = req.user || "no user";
+    console.log(user);
+    next();
+});
 
+app.get('/', function(req, res){
+    var user = req.user || "no user";
+    res.render('index', {user: user})
+});
 
 app.use('/api/flora', floraRoutes);
+
 
 app.listen(port, function(req, res){
   console.log('⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡\n⚡⚡⚡⚡ Forage Away ' + port + ' ⚡⚡⚡⚡\n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡')
