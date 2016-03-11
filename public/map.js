238f253ef3fad1528022193e34e6858c89fe2b6c
superman76 <cbloodjr@gmail.com>
2016-03-11 10:42:36 -0700

    updated map.js submitForage.ejs

--- public/map.js
+++ public/map.js
@@ -1,3 +1,5 @@
+var firebase = new Firebase("<https://Forager.firebaseIO.com>");
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 46.86077911287492, lng: -113.99279092176516},
@@ -11,4 +13,10 @@ function initMap() {
     }],
     disableDoubleClickZoom: true
   })
+  map.addListener('click', function(e) {
+  var marker = new google.maps.Marker({
+    position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+    map: map
+  });
+});
 };
