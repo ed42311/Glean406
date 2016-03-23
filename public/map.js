22bc6e807e495d30611e0a3059cb5de8c1735eb4
superman76 <cbloodjr@gmail.com>
2016-03-23 11:24:50 -0600

    refactored map.js

--- public/map.js
+++ public/map.js
@@ -44,129 +44,144 @@ function getFlora(map, category, searchText){
     type: 'GET'
   }).done(function(data) {  
 
-    console.log(data);
-    if(category === 'My Posts'){
-      var filterData = data;
-      var filterSearch = searchText ? filterData.filter(function(f){
-      var name = f.name || '';
-      var season = f.season || '';
-      var category = f.category || '';
-      var description = f.description || '';  
-      var creator = f.creator || '';
-      return  name.indexOf(searchText) > -1 || 
-              season.indexOf(searchText) > -1 ||
-              category.indexOf(searchText) > -1 ||
-              description.indexOf(searchText) > -1 ||
-              creator.indexOf(searchText) > -1;
-    }) : filterData;
-    filterSearch.forEach(function(flora) {
-      
-      var contentString = ('<div id=searchDisplayOnMap floraId=' + flora._id + '>\
-                   <p> <h3>' + flora.name + '</h3> </p>\
-                   <p> <h5>' + "Harvest Season: " + flora.season + '</h5> </p>\
-                   <p> <h5>' + "Type: " + flora.category + '</h5> </p>\
-                   <p> <h5>' + "Description: " + flora.description + '</h5> </p>\
-                   <p> <h5>' + "<button><a href=https://www.google.com/maps/dir/Current+Location/" + flora.lat + "," + flora.lng + " target=_blank>Get Directions </a></button>" +'</h5> </p>\
-                   <p> <h5>' + "<button class='floraDelete' id=" + flora._id + " onClick='deleteFlora()'>Delete Post</button>" + '</h5> </p>\
-                 </div>');
+    floraFound(data, map, category, searchText);
+  });
+}
 
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-      });
-      
-      var image = 'http://carma.org/images/icons/icon_circle_red_2.gif';
-      
-      if(flora.isPrivate == false){
-        var floraMarker = new google.maps.Marker({
-          position: {
-            lat: Number(flora.lat), 
-            lng: Number(flora.lng)
-          },
-          map: map,
-          draggable: false,
-          animation: google.maps.Animation.DROP
-        })} else {
-        var floraMarker = new google.maps.Marker({
-          position: {
-            lat: Number(flora.lat), 
-            lng: Number(flora.lng)
-          },
-          map: map,
-          icon: image,
-          draggable: false,
-          animation: google.maps.Animation.DROP,
-        })
-      }
+function floraFound(data, map, category, searchText){
 
-      floraMarker.setMap(map);
-
-      floraMarker.addListener('click', function() {
-        infowindow.open(map, floraMarker);
-      });
-    });
+  console.log(data);
+  if(category === 'My Posts'){
+    foundMyFlora(data, map, category, searchText);
   }
-});
-    var filterData = category ? data.filter(function(f){f.category === category}) : data;
+    
+  console.log(category);
 
-    var filterSearch = searchText ? filterData.filter(function(f){
-      var name = f.name || '';
-      var season = f.season || '';
-      var category = f.category || '';
-      var description = f.description || '';  
-      var creator = f.creator || '';
-      return  name.indexOf(searchText) > -1 || 
-              season.indexOf(searchText) > -1 ||
-              category.indexOf(searchText) > -1 ||
-              description.indexOf(searchText) > -1 ||
-              creator.indexOf(searchText) > -1;
-    }) : filterData;
-    filterSearch.forEach(function(flora) {
-      
+  floraNotMine(data, map, category, searchText);
+}
 
-      var contentString = ('<div id=' + flora.name + '>\
-                   <p> <h3>' + flora.name + '</h3> </p>\
-                   <p> <h5>' + "Harvest Season: " + flora.season + '</h5> </p>\
-                   <p> <h5>' + "Type: " + flora.category + '</h5> </p>\
-                   <p> <h5>' + "Description: " + flora.description + '</h5> </p>\
-                   <p> <h5>' + "<button><a href=https://www.google.com/maps/dir/Current+Location/" + flora.lat + "," + flora.lng + " target=_blank>Get Directions </a></button>" +'</h5> </p>\
-                 </div>');
+function floraNotMine(data, map, category, searchText){
+  var filterData = category ? data.filter(function(f){f.category === category}) : data;
+  var filterSearch = searchText ? filterData.filter(function(f){
+    var name = f.name || '';
+    var season = f.season || '';
+    var category = f.category || '';
+    var description = f.description || '';  
+    var creator = f.creator || '';
+    return  name.indexOf(searchText) > -1 || 
+            season.indexOf(searchText) > -1 ||
+            category.indexOf(searchText) > -1 ||
+            description.indexOf(searchText) > -1 ||
+            creator.indexOf(searchText) > -1;
+  }) : filterData;
+  filterSearch.forEach(function(flora) {
+    
+    var contentString = ('<div id=' + flora.name + '>\
+                 <p> <h3>' + flora.name + '</h3> </p>\
+                 <p> <h5>' + "Harvest Season: " + flora.season + '</h5> </p>\
+                 <p> <h5>' + "Type: " + flora.category + '</h5> </p>\
+                 <p> <h5>' + "Description: " + flora.description + '</h5> </p>\
+                 <p> <h5>' + "<button><a href=https://www.google.com/maps/dir/Current+Location/" + flora.lat + "," + flora.lng + " target=_blank>Get Directions </a></button>" +'</h5> </p>\
+               </div>');
 
-
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString,
-      });
-      
-      var image = 'http://carma.org/images/icons/icon_circle_red_2.gif';
-      
-      if(flora.isPrivate == false){
-        var floraMarker = new google.maps.Marker({
-          position: {
-            lat: Number(flora.lat), 
-            lng: Number(flora.lng)
-          },
-          map: map,
-          draggable: false,
-          animation: google.maps.Animation.DROP
-        })} else {
-        var floraMarker = new google.maps.Marker({
-          position: {
-            lat: Number(flora.lat), 
-            lng: Number(flora.lng)
-          },
-          map: map,
-          icon: image,
-          draggable: false,
-          animation: google.maps.Animation.DROP,
-        })
-      }
-
-      floraMarker.setMap(map);
-
-      floraMarker.addListener('click', function() {
-        infowindow.open(map, floraMarker);
-      });
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
     });
-  }
+    
+    var image = 'http://carma.org/images/icons/icon_circle_red_2.gif';
+    
+    if(flora.isPrivate == false){
+      var floraMarker = new google.maps.Marker({
+        position: {
+          lat: Number(flora.lat), 
+          lng: Number(flora.lng)
+        },
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+      })} else {
+      var floraMarker = new google.maps.Marker({
+        position: {
+          lat: Number(flora.lat), 
+          lng: Number(flora.lng)
+        },
+        map: map,
+        icon: image,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+      })
+    }
+
+    floraMarker.setMap(map);
+
+    floraMarker.addListener('click', function() {
+      infowindow.open(map, floraMarker);
+    });
+  });
+}
+
+
+function foundMyFlora(data, map, category, searchText){
+  var filterData = data;
+  var filterSearch = searchText ? filterData.filter(function(f){
+    var name = f.name || '';
+    var season = f.season || '';
+    var category = f.category || '';
+    var description = f.description || '';  
+    var creator = f.creator || '';
+    return  name.indexOf(searchText) > -1 || 
+            season.indexOf(searchText) > -1 ||
+            category.indexOf(searchText) > -1 ||
+            description.indexOf(searchText) > -1 ||
+            creator.indexOf(searchText) > -1;
+  }) : filterData;
+  filterSearch.forEach(function(flora) {
+    
+    var contentString = ('<div id=searchDisplayOnMap floraId=' + flora._id + '>\
+                 <p> <h3>' + flora.name + '</h3> </p>\
+                 <p> <h5>' + "Harvest Season: " + flora.season + '</h5> </p>\
+                 <p> <h5>' + "Type: " + flora.category + '</h5> </p>\
+                 <p> <h5>' + "Description: " + flora.description + '</h5> </p>\
+                 <p> <h5>' + "<button><a href=https://www.google.com/maps/dir/Current+Location/" + flora.lat + "," + flora.lng + " target=_blank>Get Directions </a></button>" +'</h5> </p>\
+                 <p> <h5>' + "<button class='floraDelete' id=" + flora._id + " onClick='deleteFlora()'>Delete Post</button>" + '</h5> </p>\
+               </div>');
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    
+    var image = 'http://carma.org/images/icons/icon_circle_red_2.gif';
+    
+    if(flora.isPrivate == false){
+      var floraMarker = new google.maps.Marker({
+        position: {
+          lat: Number(flora.lat), 
+          lng: Number(flora.lng)
+        },
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+      })
+    } else {
+      var floraMarker = new google.maps.Marker({
+        position: {
+          lat: Number(flora.lat), 
+          lng: Number(flora.lng)
+        },
+        map: map,
+        icon: image,
+        draggable: false,
+        animation: google.maps.Animation.DROP,
+      })
+    }
+
+    floraMarker.setMap(map);
+
+    floraMarker.addListener('click', function() {
+      infowindow.open(map, floraMarker);
+    });
+  });
+}
 
 $('#categoryControl').change(function() {
   var category = this.value;
@@ -182,18 +197,5 @@ $(document).ready(function(){
     initMap(category === 'All' ? undefined : category, searchText)
   })
 })
-
-
-/*
-      var markerOne = new google.maps.Marker({
-      position: {e.lat, e.lng},
-      map: map,
-      draggable: true,
-      animation: google.maps.Animation.DROP
-    });
-
-    markerOne.setMap(map);
-};
-*/
 
 
